992325f5a66baa89ed56fe85ffd9fd7b8817c6a6
DiogoFilhoDeMorais <analise.federal@gmail.com>
2025-11-24 13:02:06 -0300

    Classe Security Config

--- src/main/java/com/estudo/app/config/SecurityConfig.java
+++ src/main/java/com/estudo/app/config/SecurityConfig.java
@@ -2,7 +2,11 @@ package com.estudo.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,5 +19,15 @@ public class SecurityConfig {
         );
         http.csrf(csrf -> csrf.disable());
         return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
